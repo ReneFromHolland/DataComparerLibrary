d3442da06c1d89f238c70b1cec3b4ca779f8a699
René <datacomparerlibrary@outlook.com>
2025-03-21 21:18:40 +0100

    is_integer changed to static method

--- src/DataComparerLibrary/tools.py
+++ src/DataComparerLibrary/tools.py
@@ -1,5 +1,6 @@
 class Tools:
-    def is_integer(self, string):
+    @staticmethod
+    def is_integer(string):
         if string[0] == '-':
             # if a negative number
             return string[1:].isdigit()
